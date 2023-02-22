c46613354a4f5ddec320163b304fea73ab8c2528
Eng Operator <103228012+bp-operator@users.noreply.github.com>
2023-02-22 13:19:32 +0900

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,9 +8,9 @@ if [ ! -z $INPUT_DOCKER_NETWORK ];
 then INPUT_OPTIONS="$INPUT_OPTIONS --network $INPUT_DOCKER_NETWORK"
 fi
 
-if [ "$INPUT_SHELL" == "sh" || "$INPUT_SHELL" == "bash" ];
+if [ "$INPUT_SHELL" == "ignore" ];
 then 
-  exec docker run -v "/var/run/docker.sock":"/var/run/docker.sock" $INPUT_OPTIONS --entrypoint=$INPUT_SHELL $INPUT_IMAGE -c "${INPUT_RUN//$'\n'/;}"
-else
   exec docker run -v "/var/run/docker.sock":"/var/run/docker.sock" $INPUT_OPTIONS $INPUT_IMAGE "${INPUT_RUN//$'\n'/;}"
+else
+  exec docker run -v "/var/run/docker.sock":"/var/run/docker.sock" $INPUT_OPTIONS --entrypoint=$INPUT_SHELL $INPUT_IMAGE -c "${INPUT_RUN//$'\n'/;}"
 fi
